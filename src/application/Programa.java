e142b3e1897bbcfd6e189ac08e32925296afad6e
Davi <davi.chagas@aln.senaicimatec.edu.br>
2026-05-22 22:44:07 -0300

    Mensagem "gnz is in the house" apagada

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -3,6 +3,5 @@ package application;
 public class Programa {
     static void main(String[] args) {
         System.out.println("Bem vindo ao CareGate");
-        System.out.println("Eai, Dani Mito, gnz is in the house!");
     }
 }
